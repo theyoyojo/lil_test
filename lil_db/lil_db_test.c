038ee14e7234bd35793978929c52c35a776a8ff6
Joel Savitz <jsavitz@redhat.com>
2019-01-26 22:47:35 -0500

    began refining documentation

--- lil_db/lil_db_test.c
+++ lil_db/lil_db_test.c
@@ -14,16 +14,30 @@
 #include "lil_db.h"
 #include "test_macros.h"
 
+void foo (void (*f)(void)) {
+	(*f)() ;
+}
+
+void baz (void (*f)(void (*g)(void))) {
+	(*f)(
+		LAMBDA(void, (void)
+		{
+			printf("SUPER NESTING\n") ;
+		})
+	) ;
+}
+
 int main(int argc, char * argv[]) {
 	(void)argc,(void)argv ; // shhhhh ycm
 
 	TEST_INIT(200) ; // arbitrary value > # test cases
 
+	TEST_EXEC() ;
 	lil_db_init("xxxx",5) ;
 
 	int x = 4 ;
 
-	// Also demonstrated: capute of enclosing scope
+	// Also demonstrated: capture of enclosing scope
 	TEST_CASE(should_fail,
 		printf("%d\n",x) ;
 		ASSERT(1==2);
@@ -38,6 +52,16 @@ int main(int argc, char * argv[]) {
 			ASSERT(0) ;
 		) ;
 	) ;
+
+	foo(LAMBDA(void, (void) {
+		printf("in lambda\n") ;
+		LAMBDA(void,(void) {
+			printf("NESTY BOI\n") ;
+		})() ;
+	})) ;
+
+
+	baz(LAMBDA(void, (void(*h)(void)) { (*h)(); })) ;
 
 	TEST_EXEC() ;
 
--- lil_db/test_macros.h
+++ lil_db/test_macros.h
@@ -1,12 +1,95 @@
+/*
+ * lil_db_test_suite
+ * By Joel Savitz <jsavitz@redhat.com>
+ *
+ * Purpose: Extremely lightweight unit testing
+ *
+ */
 #ifndef TEST_MACROS_H
 #define TEST_MACROS_H
 
 #include <stdlib.h> // Literally just for exit()
 
-#define TO_STRING(x) #x
+/* UTILITY MACROS */
 
-#define LAMBDA(return_t,...) __extension__ \
-	({ return_t __fn__ __VA_ARGS__ __fn__ ; })
+ /* 
+  * ID:
+  * 		TO_STRING(x)
+  * Purpose:
+  * 		Wrapper for # operator to improve readability
+  *
+  * Inputs:
+  * 		x: An arbitrary token
+  *
+  * Resolution:
+  * 		A usable character string object contianing the text
+  * 		of the identifier
+  * 
+  */
+#define TO_STRING(identifier) #identifier
+
+ /* 
+  * ID:
+  * 		LAMBDA(return_t, ...)
+  * Purpose:
+  * 		Simple implementation of anonymous function objects
+  *
+  * Inputs:
+  * 		return_t: a valid function return type
+  *
+  * 		...	: The function body, which follows the syntax of
+  * 			 +any arbitrary function definition, begining with
+  * 			 +the tokens that immediately follow the function
+  * 			 +identifier.
+  * 			 + e.g:
+  * 			 	(parameter_t parameter_id)
+  * 			 	{
+  * 			 		arbitary ;
+  * 			 		statements ;
+  * 			 		return value ;
+  * 			 	}
+  *
+  * Resolution:
+  * 		A callable anonymous function object
+  */
+#define LAMBDA(return_t,...)						       \
+	__extension__	 		/* turn off GCC's pedantry 	    */ \
+	({				/* begin block expression  	    */ \
+	 return_t function_identifier   /* declare function id return type  */ \
+	 __VA_ARGS__			/* insert param list, function body */ \
+	 function_identifier ;		/* expression evals to function ptr */ \
+	 })				/* close block, eval to callable fn */	
+
+/* PREDICATE PROCESSING MACROS */
+
+#define TEST_FAILURE 0 		/* Returned by failing assertion */
+#define TEST_SUCCESS 1		/* Returned at end of test case control flow */
+
+#ifdef ASSERT			
+#undef ASSERT			/* Ensure usage of our custom assertion */
+#endif
+
+ /*
+  * ID:
+  *		ASSERT(predicate)
+  * Purpose:
+  * 		Process an individual comparisson in a test case
+  *
+  * Inputs:
+  * 		prediate: An arbitrary boolean logical predicate
+  *
+  * Resoltion:
+  * 		
+  */			
+#define ASSERT(predicate) \
+	if(!(predicate)) { \
+	fprintf(stderr, "In test case: %s\n"\
+		"Assertion: \"%s\" failed!\n",\
+			test_name,\
+			TO_STRING(predicate)) ;\
+	return TEST_FAILURE ; }
+
+/* CORE FUNCTIONALITY MACROS*/
 
 #define TEST_CHECK_SPACE() \
 	if (test_count_total >= test_buffspace) {\
@@ -33,21 +116,5 @@
 		test_count_passed += tests[test_counter]() ;\
 	}\
 	printf("Passed %d/%d test cases.\n",test_count_passed,test_count_total) ;}
-
-
-#define TEST_FAILURE 0
-#define TEST_SUCCESS 1
-
-#ifdef ASSERT
-#undef ASSERT
-#endif
-
-#define ASSERT(predicate) \
-	if(!(predicate)) { \
-	fprintf(stderr, "In test case: %s\n"\
-		"Assertion: \"%s\" failed!\n",\
-			test_name,\
-			TO_STRING(predicate)) ;\
-	return TEST_FAILURE ; }
 
 #endif // TEST_MACROS_H
